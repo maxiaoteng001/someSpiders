e6997c98226e0a004ab7505baf8e5cbd5019d79e
maxiaoteng <xiaoteng@bigonelab.com>
2023-01-18 10:59:31 +0800

    调整爬虫运行频率

--- src/launcher/run_spiders.py
+++ src/launcher/run_spiders.py
@@ -31,12 +31,12 @@ def crawler_tesla_used():
 def cron_job():
     sched = BlockingScheduler()
     # 每日更新热门城市地铁最新微博
-    sched.add_job(func=crawler_beijing_metro, trigger='cron', hour='6', minute='01', id='crawler_beijing_metro')
+    sched.add_job(func=crawler_beijing_metro, trigger='cron', hour='12', minute='01', id='crawler_beijing_metro')
 
     # 贝壳二手房
     sched.add_job(func=crawler_beike_ershoufang, trigger='cron', hour='7', minute='01', id='crawler_beike_ershoufang')
     
-    sched.add_job(func=crawler_tesla_used, trigger='cron', hour='11, 23', minute='05', id='crawler_tesla_used')
+    sched.add_job(func=crawler_tesla_used, trigger='cron', hour='*', minute='05', id='crawler_tesla_used')
     
     sched.start()
 
